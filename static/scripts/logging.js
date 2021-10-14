254f704802f65a9b5d662a71f2e4452a10d7562a
CogentRedTester <cogent.redtester@outlook.com>
2021-10-14 22:12:43 +1030

    implemented creation of the zip files

--- static/scripts/logging.js
+++ static/scripts/logging.js
@@ -1,9 +1,11 @@
 var logDB = {};
 
+// clear the database
 function clearLog() {
     logDB = {};
 }
 
+// parse the log from the the worker threads and save the results into the database
 function handleLogFromWorker(log) {
     if (!logDB[log.algorithm])
         logDB[log.algorithm] = {};
@@ -21,14 +23,62 @@ function handleLogFromWorker(log) {
         }) ;
     }
 }
+
+// parse the logs for a specific algorithm, and create the meta and data files in the zip
+function zipAlgorithm(zip, db, algorithm) {
+    /////////////////////////////
+    // CREATING THE META FILE  //
+    /////////////////////////////
+    zip.folder(algorithm+"/data_f1");
+    let contents = ""
+    for (const dim in db) {
+        const dimDB = db[dim];
+
+        contents += "suite = 'elea', funcId = 1, DIM = "+dim+", algId = '"+algorithm+"'\n";
+        contents += "%\n";
+        contents += "data_f1/IOHprofiler_f1_DIM"+dim+".dat";
+
+        // adding the meta-info for each run in each data file
+        for (const run in db[dim]) {
+            const runDB = dimDB[run];
+            contents += ", 1:"+runDB.max_evaluations+"|"+runDB[ runDB.length-1 ].fitness;
+        }
+        contents += "\n";
+
+        /////////////////////////////////////////
+        // CREATING THE DATA FILE FOR EACH DIM //
+        /////////////////////////////////////////
+        const header = '"function evaluation" "best-so-far f(x)"\n';
+        let contentsData = "";
+        for (const run in dimDB) {
+            contentsData += header;
     
-    logDB[log.algorithm][log.dimension][log.run].push({"evaluation": log.evaluation, "fitness": log.fitness});
+            // we can't use 'for (const in ...' because the run object also contains a max_evaluations field
+            for (let i = 0; i < dimDB[run].length; i++) {
+                const element = dimDB[run][i];
+                contentsData += element.evaluation+" "+element.fitness+"\n";
+            }
+        }
+        zip.file(algorithm+"/data_f1/IOHprofiler_f1_DIM"+dim+".dat", contentsData);
+        /////////////////////////////////////////
+        /////////////////////////////////////////
+    }
+
+    zip.file(algorithm+"/IOHprofiler_f1.info", contents);
 }
 
+// compile the zip file and give it to the user as a download
 async function downloadLog() {
+    console.warn("creating zip archive in IOHprofiler format");
     var zip = new JSZip();
     zip.file("raw.json", JSON.stringify(logDB));
 
+    for (const algorithm in logDB) {
+        zip.folder(algorithm);
+        zipAlgorithm(zip, logDB[algorithm], algorithm);
+    }
+
     let file = await zip.generateAsync({type: "blob"});
     downloadFile(file, "log.zip");
+    if (file) clearLog();
 }
