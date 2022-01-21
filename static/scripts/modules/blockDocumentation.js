b73b4b3795891786dc010a202c79ba197b95d973
Antony Kamp <tony.kamp@web.de>
2022-01-21 15:18:34 +0100

    extract information from json object module

---
+++ static/scripts/modules/blockDocumentation.js
@@ -0,0 +1,72 @@
+import * as Blockly from "blockly";
+
+function get_block_name(json_object) {
+  let block_name = "";
+  let num_args = 1;
+  for (let substr in json_object.message0.split(" ")) {
+    if (!substr.match(/%\d+/g)) {
+      block_name.concat(substr + " ");
+    } else if (
+      json_object.args0[parseInt(x.substring(1)) - 1].type == "input_dummy"
+    ) {
+      block_name.concat("%" + num_args++ + " ");
+    }
+  }
+  return block_name;
+}
+
+function get_tooltip(json_object) {
+  return json_object.tooltip || false;
+}
+
+function get_category(json_object) {
+  return json_object.category || "moreblocks";
+}
+
+function get_output_type(json_object) {
+  return json_object.output || false;
+}
+
+function get_input(json_object) {
+  let inputs = [];
+  for (let input in json_object.args0) {
+    if (input.type == "input_dummy")
+      inputs.push({
+        name: input.name,
+        type: input.type,
+        comment: input.comment || false,
+      });
+  }
+  return inputs;
+}
+
+function set_block_image(json_object, div_element) {
+  var workspace = Blockly.inject(div_element, {
+    comments: false,
+    toolbox: false,
+    trashcan: false,
+    readOnly: true,
+    scrollbars: false,
+    zoom: false,
+  });
+
+  Blockly.Blocks["math_change"] = {
+    init: function () {
+      this.jsonInit(json_object);
+    },
+  };
+
+  var metrics = workspace.getMetrics();
+  div_element.style.height = metrics.contentHeight + "px";
+  div_element.style.width = metrics.contentWidth + "px";
+  Blockly.svgResize(workspace);
+}
+
+export {
+  get_block_name,
+  get_tooltip,
+  get_category,
+  get_input,
+  get_output_type,
+  set_block_image,
+};
