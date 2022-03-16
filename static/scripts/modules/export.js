e6449c29ef25a537eafa68148c6f8cd3931e7ac5
Antony Kamp <tony.kamp@web.de>
2022-03-16 18:44:20 +0100

    export zip

--- static/scripts/modules/export.js
+++ static/scripts/modules/export.js
@@ -2,6 +2,7 @@ import * as Blockly from "blockly";
 import { resetHasUnsavedChangesHandling } from "./unsavedChangesHandling";
 import { workspace, getCode } from "./blocklyHandling";
 import { downloadFile, copyToClipboard } from "./fileUtils";
+import JSZip from "../jszip";
 
 function copyXMLToClipboard() {
   var xml = Blockly.Xml.domToPrettyText(Blockly.Xml.workspaceToDom(workspace));
@@ -26,9 +27,79 @@ function downloadWorkspace() {
   download(xml, "algorithm.xml", "text/xml");
 }
 
-function downloadWorkspaceAsJS() {
-  var js = getCode();
-  download(js, "algorithm.js", "text/javascript");
+async function downloadWorkspaceAsJS() {
+  let algorithm = await prepare_algorithm();
+  let message_handler = await prepare_messagerhandler();
+  let readme = await read_file("./export/README.md");
+  let main = await read_file("./export/main.mjs");
+  let logging = await read_file("./scripts/modules/logging.mjs");
+  if (
+    ![algorithm, message_handler, readme, main, logging].every(
+      (f) => f != false
+    )
+  ) {
+    alert("Something went wrong, please try again.");
+    return;
+  }
+  let zip = new JSZip();
+  zip.file("algorithm.js", algorithm);
+  zip.file("MessageHandler.js", message_handler);
+  zip.file("README.md", readme);
+  zip.file("main.mjs", main);
+  zip.file("logging.mjs", logging);
+  let zip_file = await zip.generateAsync({ type: "blob" });
+  downloadFile(zip_file, "elea.zip");
+}
+
+async function prepare_messagerhandler() {
+  let file;
+  if (!(file = await read_file("./scripts/MessageHandler.js"))) return false;
+  let code =
+    `const { Worker, parentPort } = require("worker_threads");\n` +
+    file +
+    `// redirect messages from the parent to the message handler\n` +
+    `// labels the source as having an ID of 0 - the parent's ID\n` +
+    `parentPort.onmessage = function (msg) {\n` +
+    `msg.data.source = Handler.PARENT_ID;\n` +
+    `Handler.handleIncomingMessage(msg.data);\n` +
+    `};\n` +
+    `module.exports = {\n` +
+    ` Message,\n` +
+    ` MessageHandler,\n` +
+    ` consolelog,\n` +
+    ` consoleerror,\n` +
+    ` Handler,\n` +
+    ` RecvRequest,\n` +
+    `};`;
+  return code;
+}
+
+function prepare_algorithm() {
+  let setup =
+    `const {parentPort, Worker} = require("worker_threads");\n` +
+    `const {Handler, consolelog, consoleerror, Message, RecvRequest} = require("./MessageHandler.js");\n` +
+    `const {cpus} = require("os");\n` +
+    `Handler.setParentPort(parentPort);\n`;
+
+  let js = getCode();
+  let var_declaration = js
+    .split("\n")
+    .shift()
+    .replace("var", "")
+    .replace(";", "")
+    .split(" ")
+    .join(" globalThis.");
+  js = js.split("\n");
+  js.shift();
+  js = js.join("\n");
+  let tmp = setup + var_declaration + "\n" + js;
+  return tmp;
+}
+
+async function read_file(path) {
+  let response = await fetch(path);
+  if (!response.ok) return false;
+  return await response.text();
 }
 
 export {
