7bd32ae6b555d8466a108887a34b1553c8cbde16
Antony Kamp <tony.kamp@web.de>
2022-03-16 11:42:08 +0100

    add "start-script" for exported javascript

---
+++ static/export/main.mjs
@@ -0,0 +1,38 @@
+import {handleLogFromWorker} from "./logging.mjs";
+import { Worker }  from "worker_threads";
+var worker = new Worker("./algorithm.js");
+worker.on("message", handleMessageFromWorker);
+worker.on("error", handleErrorFromWorker);
+function handleMessageFromWorker(msg){
+    if (msg.ctrl == "print") {
+        if (msg.source)
+            console.log("[Thread#" + msg.sources.join(".") + "] " + msg.data + "\n")
+        else
+            console.log(msg.data)
+        return;
+    }
+
+    if (msg.ctrl == "log") {
+        handleLogFromWorker(msg.data);
+        return;
+    }
+
+    // this is sent by the worker when the main function returns
+    if (msg.ctrl == "terminate") {
+        console.log("terminate worker due to its request.");
+        terminateWorker();
+        return;
+    }
+}
+
+function handleErrorFromWorker(err){
+    terminateWorker()
+    throw new Error(err.stack)
+}
+
+function terminateWorker(){
+    if (worker != null) {
+        worker.terminate();
+        console.warn("Terminated running worker");
+    }
+}
