fad315cd7ee4cace2466eb2a07c290b8f1f5816c
Antony Kamp <tony.kamp@web.de>
2022-05-30 16:54:56 +0200

    add comments to unsavedChangesHandling

--- static/scripts/modules/unsavedChangesHandling.js
+++ static/scripts/modules/unsavedChangesHandling.js
@@ -2,6 +2,8 @@ import * as Blockly from "blockly";
 import { workspace } from "./blocklyHandling";
 
 var HAS_UNSAVED_CHANGES = false;
+
+// Change operations of Blockly that indicates an relevant one
 const CHANGE_OPERATIONS = [
   Blockly.Events.BLOCK_CHANGE,
   Blockly.Events.BLOCK_CREATE,
@@ -15,6 +17,10 @@ const CHANGE_OPERATIONS = [
   Blockly.Events.COMMENT_CHANGE,
   Blockly.Events.COMMENT_MOVE,
 ];
+
+// This function waits for a relevant change
+// and updates the status, indicator for unsaved changes
+// and sets up an listener for unloading the workspace.
 function unsavedChangesListener(event) {
   if (CHANGE_OPERATIONS.includes(event.type)) {
     HAS_UNSAVED_CHANGES = true;
@@ -26,11 +32,16 @@ function unsavedChangesListener(event) {
   }
 }
 
+// If the user loads a new workspace or leaves the page,
+// this function asks the user if he or she wants to discard
+// unsaved changes
 function beforeUnloadListener(e) {
   e.preventDefault();
   return (e.returnValue = "Are you sure you want to exit without saving?");
 }
 
+// when the user saves the workspace or the workspace was loaded,
+// this function resets the "unsaved changes"-indicator.
 function resetHasUnsavedChangesHandling(finishedLoading = false) {
   if (HAS_UNSAVED_CHANGES | finishedLoading) {
     HAS_UNSAVED_CHANGES = false;
@@ -43,6 +54,11 @@ function resetHasUnsavedChangesHandling(finishedLoading = false) {
   }
 }
 
+// changes that happens when the workspace was loaded shouldn't
+// be recognized as changes by the user. That's why the detection
+// for unsaved changes will be suspended until the workspace was loaded.
+// This function initiates the listener for unsaved changes
+// after the workspace was loaded.
 function waitForFinishedLoading(event) {
   if (event.type == Blockly.Events.FINISHED_LOADING) {
     resetHasUnsavedChangesHandling(true);
@@ -50,6 +66,10 @@ function waitForFinishedLoading(event) {
   }
 }
 
+// changes, that happens when the workspace was loaded, shouldn't
+// be recognized as changes by the user. That's why the detection
+// for unsaved changes will be suspended until the workspace was loaded.
+// This function sets up a listener for this event.
 function setupUnsavedChangesHandling() {
   workspace.addChangeListener(waitForFinishedLoading);
 }
