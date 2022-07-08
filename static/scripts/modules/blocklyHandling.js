2324231c5f353615069837c54a4214951f929f2b
kohlros <kohlros@uni-potsdam.de>
2022-07-08 08:49:35 +0200

    fix merge

--- static/scripts/modules/blocklyHandling.js
+++ static/scripts/modules/blocklyHandling.js
@@ -206,7 +206,6 @@ function handleMessageFromWorker(msg) {
   if (msg.ctrl == "terminate") {
     console.log("terminate worker due to its request.");
     terminateWorker();
-    printDoneMessage();
     printDoneMessageIOH();
     printDoneMessageCSV();
     drawPlots();
