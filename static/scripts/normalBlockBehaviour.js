ee4c30121537819d4a7d00192b30812a08e41d43
Gerome Quantmeyer <106745614+geromequa@users.noreply.github.com>
2022-08-06 11:20:11 +0200

    fixed variable declaration
    lint check didnt work. had to change code variable declaration (trivial)

--- static/scripts/normalBlockBehaviour.js
+++ static/scripts/normalBlockBehaviour.js
@@ -882,18 +882,19 @@ Blockly.JavaScript['iteration_counter_loop'] = function(block) {
   var variableCondition = Blockly.JavaScript.valueToCode(block, 'loop_condition', Blockly.JavaScript.ORDER_ATOMIC);
   var statements = Blockly.JavaScript.statementToCode(block, 'loop_statement');
   var variableCounter = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('counter_variable'), Blockly.Variables.NAME_TYPE);
-
+  
+  var code;
   if (variableLoopMode == "while"){
-    var code = "var " + variableCounter + " = 0;\n";
-  code += variableLoopMode + "(" + variableCondition +"){\n";
-  code += statements + variableCounter + "++;\n";
-  code += "}\n"
+    code += "var " + variableCounter + " = 0;\n";
+    code += variableLoopMode + "(" + variableCondition +"){\n";
+    code += statements + variableCounter + "++;\n";
+    code += "}\n"
   return code;
   } else {
-    var code = "var " + variableCounter + " = 0;\n";
-  code += variableLoopMode + "(!" + variableCondition +"){\n";
-  code += statements + variableCounter + "++;\n";
-  code += "}\n"
+    code += "var " + variableCounter + " = 0;\n";
+    code += variableLoopMode + "(!" + variableCondition +"){\n";
+    code += statements + variableCounter + "++;\n";
+    code += "}\n"
   return code;
   }
 };
